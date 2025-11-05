b20d07de004241c5950711607db6ba533f995769
IgniteCoders <theboom.kajas@gmail.com>
2025-11-05 11:08:30 +0100

    Added description and colors

--- IMC-iOS/MainViewController.swift
+++ IMC-iOS/MainViewController.swift
@@ -16,6 +16,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var heightSlider: UISlider!
     
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     var weight: Int = 70
     var height: Int = 175
@@ -24,8 +25,8 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        weightLabel.text = "\(weight)"
-        heightLabel.text = "\(height)"
+        weightLabel.text = "\(weight) kg"
+        heightLabel.text = "\(height) cm"
         
         weightStepper.value = Double(weight)
         heightSlider.value = Float(height)
@@ -35,14 +36,14 @@ class MainViewController: UIViewController {
     
     @IBAction func onChangeWeight(_ sender: UIStepper) {
         weight = Int(sender.value)
-        weightLabel.text = "\(weight)"
-        //calculateBMI()
+        weightLabel.text = "\(weight) kg"
+        calculateBMI()
     }
     
     @IBAction func onChangeHeight(_ sender: UISlider) {
         height = Int(sender.value)
-        heightLabel.text = "\(height)"
-        //calculateBMI()
+        heightLabel.text = "\(height) cm"
+        calculateBMI()
     }
     
     @IBAction func calculateBMI() {
@@ -51,6 +52,29 @@ class MainViewController: UIViewController {
         let result = Double(weight) / pow(heightInMeters, 2)
         
         resultLabel.text = String(format: "%.2f", result)
+        
+        switch result {
+        case 0..<18.5:
+            descriptionLabel.text = "Underweight"
+            descriptionLabel.textColor = UIColor.bmiColorUnderweight
+            resultLabel.textColor = UIColor.bmiColorUnderweight
+        case 18.5..<25.0:
+            descriptionLabel.text = "Normal weight"
+            descriptionLabel.textColor = UIColor.bmiColorNormal
+            resultLabel.textColor = UIColor.bmiColorNormal
+        case 25..<30:
+            descriptionLabel.text = "Overweight"
+            descriptionLabel.textColor = UIColor.bmiColorOverweight
+            resultLabel.textColor = UIColor.bmiColorOverweight
+        case 30..<40:
+            descriptionLabel.text = "Obesity"
+            descriptionLabel.textColor = UIColor.bmiColorObesity
+            resultLabel.textColor = UIColor.bmiColorObesity
+        default:
+            descriptionLabel.text = "Extreme obesity"
+            descriptionLabel.textColor = UIColor.bmiColorExtremeObesity
+            resultLabel.textColor = UIColor.bmiColorExtremeObesity
+        }
     }
 }
 
